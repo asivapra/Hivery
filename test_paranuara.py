516142878a601a458f8e3b92914c7500dbb10b1d
Arapaut V Sivaprasad <avs@webgenie.com>
2020-05-18 12:28:52 +1000

    Update test_paranuara.py

--- test_paranuara.py
+++ test_paranuara.py
@@ -7,6 +7,7 @@ class MyTestCase(unittest.TestCase):
         stream = os.popen('curl -s curl http://127.0.0.1:8080/companyName/LINGOAGE')
         output = stream.read()
         stream.close()
+        print("Test1: Employees in company, LINGOAGE:", output)
         exp_output = '["Sue Tyson","Hobbs Lang","Shelly Koch","Santiago Baker",' \
                      '"Lolita Walls","Shari Farrell","Gordon Wolfe"]\n'
         self.assertEqual(output, exp_output)
@@ -15,6 +16,7 @@ class MyTestCase(unittest.TestCase):
         stream = os.popen('curl -s curl http://127.0.0.1:8080/companyName/LINGOAGE_NOTEXIST')
         output = stream.read()
         stream.close()
+        print("Test2: Non-existing company, LINGOAGE_NOTEXIST:", output)
         exp_output = '\"Company is not found\"\n'
         self.assertEqual(output, exp_output)
 
@@ -22,6 +24,7 @@ class MyTestCase(unittest.TestCase):
         stream = os.popen('curl -s curl curl http://127.0.0.1:8080/companyName/BOVIS')
         output = stream.read()
         stream.close()
+        print("Test3: No employees in company, BOVIS:", output)
         exp_output = '\"Company has no employees\"\n'
         self.assertEqual(output, exp_output)
 
@@ -29,6 +32,7 @@ class MyTestCase(unittest.TestCase):
         stream = os.popen('curl -s http://127.0.0.1:8080/persons/Lila%20Gray,Elinor%20Wiggins')
         output = stream.read()
         stream.close()
+        print("Test4: Common friends between two persons, Lila Gray and Elinor Wiggins:", output)
         exp_output = '[{"address":"477 Amersfort Place, Rivera, Nebraska, 2569",' \
                      '"age":21,"name":"Lila Gray","phone":"+1 (883) 414-3615"},' \
                      '{"address":"625 Oxford Street, Rosedale, Palau, 6678",' \
@@ -44,6 +48,7 @@ class MyTestCase(unittest.TestCase):
         stream = os.popen('curl -s http://127.0.0.1:8080/favouriteFoods/Booth%20Haynes')
         output = stream.read()
         stream.close()
+        print("Test5: Favourite fruits and vegetables of a person, Booth Haynes:", output)
         exp_output = '{"age":46,"fruits":["apple","strawberry"],"username":"Booth Haynes",' \
                      '"vegetables":["cucumber","carrot"]}\n'
         self.assertEqual(output, exp_output)
